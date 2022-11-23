1e1cb32f953736dc797a8bb8468c3ba13b83583f
Gabriel <gabrieljssouza@gmail.com>
2022-11-23 19:00:04 -0300

    Adiciona estrutura condicional

--- script.js
+++ script.js
@@ -21,3 +21,10 @@ console.log(divisao);
 
 const modulo = a % b;
 console.log(modulo);
+
+const number1 = 10;
+const number2 = 20;
+if (
+  number1 > number2
+) {console.log('number1 é maior que number2')}
+else {console.log('number2 é maior que number1')};
