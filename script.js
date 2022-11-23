209a7318754a1852f009bd047ba19a09f03eedcd
Gabriel <gabrieljssouza@gmail.com>
2022-11-23 18:52:16 -0300

    Remove codigos antigos, adiciona operadores aritimeticos

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const myName = 'Gabriel';
-const birthCity = 'Porto Alegre';
-console.log(myName);
-console.log(birthCity);
+//Adição (a + b)
+//Subtração (a - b)
+//Multiplicação (a * b)
+//Divisão (a / b)
+//Módulo (a % b) 
+
+let a = 2;
+let b = 60;
+
+const adicao = a + b;
+console.log(adicao);
+
+const subtracao = a - b;
+console.log(subtracao);
+
+const multiplicacao = a * b;
+console.log(multiplicacao);
+
+const divisao = a / b;
+console.log(divisao);
+
+const modulo = a % b;
+console.log(modulo);
