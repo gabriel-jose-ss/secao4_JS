9a3f68539026073dc09bcb6988db48e069cf7144
Gabriel <gabrieljssouza@gmail.com>
2022-11-23 19:19:59 -0300

    Adiciona estrutura condicional que condiciona se o numero é positivo negativo ou ainda se nao é 0

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //Subtração (a - b)
 //Multiplicação (a * b)
 //Divisão (a / b)
-//Módulo (a % b) 
+//Módulo (a % b)
 
 let a = 2;
 let b = 60;
@@ -24,21 +24,27 @@ console.log(modulo);
 
 const number1 = 10;
 const number2 = 20;
-if (
-  number1 > number2
-) {console.log('number1 é maior que number2')}
-else {console.log('number2 é maior que number1')};
+if (number1 > number2) {
+  console.log("number1 é maior que number2");
+} else {
+  console.log("number2 é maior que number1");
+}
 
 const n1 = 654;
 const n2 = 466;
 const n3 = 155;
 
 if (n1 > n2 && n1 > n3) {
-  console.log('O maior número é ' + n1 + '(n1)') 
+  console.log("O maior número é " + n1 + "(n1)");
+} else if (n2 > n1 && n2 > n3) {
+  console.log("O maior número é " + n2 + "(n2)");
+} else {
+  console.log("O maior número é " + n3 + "(n3)");
 }
-else if (n2 > n1 && n2 > n3) {
-  console.log('O maior número é ' + n2 + '(n2)')
-}
-else {
-  console.log('O maior número é ' + n3 + '(n3)')
-};
+
+let negative = -466;
+if (negative < 0)
+{console.log('negative') }
+else if (negative >= 1) 
+{console.log('positive')}
+else {console.log('zero')};
